0158c8318923800aa1607a5921581cbb206ebb29
Leticia Fontes <leticia.abib95@gmail.com>
2025-06-26 14:49:40 -0300

    versão comp leticia

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { UsuarioModule } from './usuario/usuario.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'samueladmin',
+      password: 'root',
       database: 'db_genFitness',
       entities: [Usuario],
       synchronize: true,
